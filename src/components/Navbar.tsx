48d6487780d0c73cee2d71aef5a9dfe25047e729
Komarala <komaralahemanth@gmail.com>
2026-04-23 10:35:53 +0200

    chore: product logo check

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar({ theme, onToggleTheme }: NavbarProps) {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <img src="public/logo.png" alt="Karl Coffee Logo" className="navbar-logo" />
+        <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Karl Coffee Logo" className="navbar-logo" />
         <div>
           <div className="navbar-title">Anfragen- und Angebotsmanagement</div>
         </div>
